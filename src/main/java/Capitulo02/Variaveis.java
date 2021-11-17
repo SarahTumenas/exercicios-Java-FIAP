21154eb323c0fe260a7a6ec4be066d3acfee0159
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-17 15:41:04 -0300

    Variaveis

---
+++ src/main/java/Capitulo02/Variaveis.java
@@ -0,0 +1,20 @@
+package Capitulo02;
+
+public class Variaveis {
+
+    public static void main(String[] args) {
+
+        int idade =10;
+        double preco = 10.0;
+        char sexo = 'F';
+        boolean temFilhos = true;
+
+        System.out.println(idade);
+
+        System.out.println(preco);
+
+        System.out.println(sexo);
+
+        System.out.println(temFilhos);
+    }
+}
