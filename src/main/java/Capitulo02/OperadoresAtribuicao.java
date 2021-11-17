e6372756d5da47c197c27ce6e77ff1a3042744bf
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-17 16:32:07 -0300

    operarodores atribuição

---
+++ src/main/java/Capitulo02/OperadoresAtribuicao.java
@@ -0,0 +1,30 @@
+package Capitulo02;
+
+public class OperadoresAtribuicao {
+    public static void main(String[] args) {
+
+        int w = 10;
+        w = w + 15;
+
+        System.out.println(w);
+
+        int z = 10;
+        z += 15;
+
+        System.out.println(z);
+
+        z *= 10;
+        System.out.println(z);
+
+        z -=5;
+        System.out.println(z);
+
+        z /= 2;
+        System.out.println(z);
+
+        z %= 3;
+
+        System.out.println(z);
+
+    }
+}
