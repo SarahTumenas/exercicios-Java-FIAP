38b955818286f89a54cd6ade13ffca8e81ae50b0
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-17 16:50:34 -0300

    operadores relacionais

---
+++ src/main/java/Capitulo02/OperadoresRelacionais.java
@@ -0,0 +1,14 @@
+package Capitulo02;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+
+        int idade = 20;
+        boolean maioridade = idade >= 18;
+
+        System.out.println(maioridade);
+
+
+
+    }
+}
