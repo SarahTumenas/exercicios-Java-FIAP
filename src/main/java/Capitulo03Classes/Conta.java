8f64ac7388f7fd599a5545f68d706648ddb0f3d9
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-19 17:01:11 -0300

    Classes

--- src/main/java/Capitulo03Classes/Conta.java
+++ src/main/java/Capitulo03Classes/Conta.java
@@ -2,9 +2,49 @@ package Capitulo03Classes;
 
 public class Conta {
 
-    int numero;
-    double saldo;
+    private int numero;
+    int agencia;
+    private double saldo;
+
+    public Conta (){
+
+    }
+
+
+    public Conta (int numero, double saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+
+    }
 
     Cliente cliente = new Cliente();
+
+    public double recuperarSaldo (){
+        return saldo;
+    }
+
+    public void depositar (double valor){
+        saldo = saldo + valor;
+    }
+
+    public void retirar (double valor){
+        saldo = saldo - valor;
+    }
+
+    public void retirar (double valor, double taxa){
+        saldo = saldo - valor -taxa;
+    }
+
+    public void setAgencia (int agencia){
+        this.agencia = agencia;
+    }
+
+    public void setNumero (int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero (){
+        return numero;
+    }
 
 }
--- src/main/java/Capitulo03Classes/TesteConta.java
+++ src/main/java/Capitulo03Classes/TesteConta.java
@@ -5,26 +5,32 @@ public class TesteConta {
         Conta contaCorrente = new Conta ();
         Conta contaPoupanca = new Conta ();
         Conta contaInvestimento = new Conta ();
+        Conta cc = new Conta(1000, 50.0);
 
-        contaCorrente.numero = 11;
-        contaCorrente.saldo = 20.20;
+
+
+        //contaCorrente.numero = 11;
+        //contaCorrente.saldo = 20.20;
         contaCorrente.cliente.nome = "Leandro";
         contaCorrente.cliente.idade = 36;
 
-        contaPoupanca.numero =  22;
-        contaPoupanca.saldo =   300;
+       // contaPoupanca.numero =  22;
+       // contaPoupanca.saldo =   300;
         contaPoupanca.cliente.nome = "Marcos";
         contaPoupanca.cliente.idade = 55;
 
-        contaInvestimento.numero = 33;
-        contaInvestimento.saldo = 1500;
+        //contaInvestimento.numero = 33;
+        //contaInvestimento.saldo = 1500;
         contaInvestimento.cliente.nome = "Marcia";
         contaInvestimento.cliente.idade = 41;
 
-        System.out.println(contaCorrente.numero);
-        System.out.println(contaCorrente.saldo);
+        //System.out.println(contaCorrente.numero);
+        //System.out.println(contaCorrente.saldo);
         System.out.println(contaCorrente.cliente.nome);
         System.out.println(contaCorrente.cliente.idade);
+
+        cc.setNumero(1000);
+        System.out.println(cc.recuperarSaldo());
 
 
     }
